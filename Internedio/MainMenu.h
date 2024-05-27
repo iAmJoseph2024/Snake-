9ee1c6dbca7271ac91d391b36b7450b82d083394
Rebecca <164688362+Reb3cc4@users.noreply.github.com>
2024-05-27 09:56:31 -0700

    Update and rename MainMenu.h to Internedio/MainMenu.h

--- Internedio/MainMenu.h
+++ Internedio/MainMenu.h
@@ -2,21 +2,21 @@
 #include <memory> // Incluimos la libreria para std::shared_ptr
 #include <SFML/Graphics/Text.hpp> // Incluimos la libreria para sf::Text
 #include "Game.h" // Incluimos la libreria creada por nosotros para la clase Game
-#include "State.h" // Incluimos la librerÌa creada por nosotros para la clase State
+#include "State.h" // Incluimos la librer√≠a creada por nosotros para la clase State
 
 // Definimos una clase llamada MainMenu que hereda de la clase State del namespace Engine
 class MainMenu : public Engine::State {
 private:
     std::shared_ptr<Context> m_context; // Puntero compartido a un objeto de tipo Context
-    sf::Text m_gameTitle; // Texto para el tÌtulo del juego
-    sf::Text m_playButton; // Texto para el botÛn de jugar
-    sf::Text m_exitButton; // Texto para el botÛn de salir
+    sf::Text m_gameTitle; // Texto para el t√≠tulo del juego
+    sf::Text m_playButton; // Texto para el bot√≥n de jugar
+    sf::Text m_exitButton; // Texto para el bot√≥n de salir
 
-    bool m_isPlayButtonSelected; // Variable que indica si el botÛn de jugar est· seleccionado
-    bool m_isPlayButtonPressed; // Variable que indica si el botÛn de jugar est· presionado
+    bool m_isPlayButtonSelected; // Variable que indica si el bot√≥n de jugar est√° seleccionado
+    bool m_isPlayButtonPressed; // Variable que indica si el bot√≥n de jugar est√° presionado
 
-    bool m_isExitButtonSelected; // Variable que indica si el botÛn de salir est· seleccionado
-    bool m_isExitButtonPressed; // Variable que indica si el botÛn de salir est· presionado
+    bool m_isExitButtonSelected; // Variable que indica si el bot√≥n de salir est√° seleccionado
+    bool m_isExitButtonPressed; // Variable que indica si el bot√≥n de salir est√° presionado
 
 public:
     // Constructor de la clase MainMenu que recibe un puntero compartido a un objeto de tipo Context
